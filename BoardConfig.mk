3fa2d405a4c6079ab8b1e3a9dc384000c0b93d76
Hyejin Kim <hyejin.kim@lge.com>
2015-03-06 17:35:08 +0900

    Set shader cache configuration options
    Set the maximum shader cache size and key size that will be used by Open GL.
    
    Change-Id: I37e68cc823ba4d081b9a9cdcd02f932174528c57

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -51,6 +51,16 @@ TARGET_NO_RPC := true
 
 BOARD_EGL_CFG := device/lge/bullhead/egl.cfg
 
+# Shader cache config options
+# Maximum size of the  GLES Shaders that can be cached for reuse.
+# Increase the size if shaders of size greater than 12KB are used.
+MAX_EGL_CACHE_KEY_SIZE := 12*1024
+
+# Maximum GLES shader cache size for each app to store the compiled shader
+# binaries. Decrease the size if RAM or Flash Storage size is a limitation
+# of the device.
+MAX_EGL_CACHE_SIZE := 2048*1024
+
 USE_OPENGL_RENDERER := true
 TARGET_USES_ION := true
 TARGET_USES_C2D_COMPOSITION := true
