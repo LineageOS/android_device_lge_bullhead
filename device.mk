c163a0c655705d29eece59131b6a8ac199ced66d
Devin Kim <dojip.kim@lge.com>
2015-03-06 19:43:33 -0800

    Use prebulit kernel image
    Change-Id: I38f536340e72cd74d2809eac3017a4e0b0fd5576

--- device.mk
+++ device.mk
@@ -21,7 +21,7 @@
 
 
 ifeq ($(TARGET_PREBUILT_KERNEL),)
-LOCAL_KERNEL := device/lge/bullhead-kernel/zImage-dtb
+LOCAL_KERNEL := device/lge/bullhead-kernel/Image-dtb
 else
 LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
 endif
