eb084bbc875994dfce76227263b2aa7f7363917b
Patrick Tjin <pattjin@google.com>
2015-03-23 13:33:57 -0700

    bullhead: clean up build post-merge
    Cleans up overlay/frameworks/base/core/res/res/values/config.xml
    Fixes build error in device.mk, liblight/lights.c
    
    Change-Id: I1bf0e3b9d30778f3ffe8c20fbcf6e1defa1b4f7c
    Signed-off-by: Patrick Tjin <pattjin@google.com>

--- device.mk
+++ device.mk
@@ -62,7 +62,7 @@ PRODUCT_COPY_FILES += \
 #    frameworks/native/data/etc/android.hardware.bluetooth_le.xml:system/etc/permissions/android.hardware.bluetooth_le.xml
 # Temporary workaround until we get bluetooth working
 PRODUCT_COPY_FILES += \
-    device/lge/bullhead/handheld_core_hardware_nobt.xml:system/etc/permissions/handheld_core_hardware.xml
+    device/lge/bullhead/handheld_core_hardware_nobt.xml:system/etc/permissions/handheld_core_hardware.xml \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
     frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
     frameworks/native/data/etc/android.hardware.camera.full.xml:system/etc/permissions/android.hardware.camera.full.xml \
