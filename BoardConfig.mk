95ac445dcc02f69e881cf47716bba4aaf6d7c00d
Patrick Tjin <pattjin@google.com>
2015-04-03 09:21:01 -0700

    bullhead: Fix BOARD_MKBOOTIMG_ARGS
    Change-Id: I9ca2700c10d43380ccc1b8498765821a83da6c45

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -37,7 +37,7 @@ BOARD_KERNEL_TAGS_OFFSET := 0x01E00000
 BOARD_RAMDISK_OFFSET     := 0x02000000
 
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=bullhead androidboot.selinux=permissive boot_cpus=0-5
-BOARD_MKBOOTIMG_ARGS := --ramdisk_offset BOARD_RAMDISK_OFFSET --tags_offset BOARD_KERNEL_TAGS_OFFSET
+BOARD_MKBOOTIMG_ARGS := --ramdisk_offset $(BOARD_RAMDISK_OFFSET) --tags_offset $(BOARD_KERNEL_TAGS_OFFSET)
 
 BOARD_USES_ALSA_AUDIO := true
 
