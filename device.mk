3065040b5ac795496758b259672c4e2c61ce4dd8
Sungmin Choi <sungmin.choi@lge.com>
2015-05-17 23:13:22 +0900

    Bluetooth: update transport initialization timeout property
    original commit:
    commit 59a3c9bc526432c1363cd5d8a12d7651a451937b
    Author: Bhakthavatsala Raghavendra <braghave@codeaurora.org>
    Date:   Sun Apr 19 22:33:07 2015 -0700
    
        Bluetooth: update transport initialization timeout property
    
        Change-Id: Ia03692b62403b4a023cba47836c5df289fceaafa
    
    Change-Id: I1378be6e565c2113248fc3e64c99a7a6d13c2a75

--- device.mk
+++ device.mk
@@ -286,6 +286,10 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     qcom.bluetooth.soc=rome
 
+# Set Bluetooth transport initialization timeout
+PRODUCT_PROPERTY_OVERRIDES += \
+    bluetooth.enable_timeout_ms=12000
+
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     ro.bt.bdaddr_path=/persist/bdaddr.txt
 
