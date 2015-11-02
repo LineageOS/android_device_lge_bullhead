6c268eb437698277bb1a561f9dbcc075fb570514
Adnan Begovic <adnan@cyngn.com>
2015-11-02 15:32:47 -0800

    bullhead: Remove vendor files restriction.
    Change-Id: Ice17b936d43f8003e809b4669536787f4f225837

--- aosp_bullhead.mk
+++ aosp_bullhead.mk
@@ -29,7 +29,7 @@ PRODUCT_DEVICE := bullhead
 PRODUCT_BRAND := Android
 PRODUCT_MODEL := AOSP on BullHead
 PRODUCT_MANUFACTURER := LGE
-PRODUCT_RESTRICT_VENDOR_FILES := true
+PRODUCT_RESTRICT_VENDOR_FILES := false
 
 #PRODUCT_COPY_FILES += device/lge/bullhead/fstab.aosp_bullhead:root/fstab.bullhead
 
