139847a6cab8350416a48bb5faff8962eeeedde4
Jake Whatley <jacob.whatley91@gmail.com>
2015-11-03 11:15:39 -0500

    bullhead: setup lineage makefiles
    Change-Id: I3882a8381338427364688bd5e372a07f19d02e92

---
+++ lineage.mk
@@ -0,0 +1,20 @@
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1920
+TARGET_SCREEN_WIDTH := 1080
+
+# Inherit some common LineageOS stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/lge/bullhead/aosp_bullhead.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_NAME := lineage_bullhead
+PRODUCT_BRAND := google
+PRODUCT_MODEL := Nexus 5X
+TARGET_MANUFACTURER := LGE
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=bullhead \
+    BUILD_FINGERPRINT="google/bullhead/bullhead:8.0.0/OPR6.170623.013/4283548:user/release-keys" \
+    PRIVATE_BUILD_DESC="bullhead-user 8.0.0 OPR6.170623.013 4283548 release-keys"
