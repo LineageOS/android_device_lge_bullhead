2ac7ecaed128bd064529044c5398ab63c0cf3eee
Jake Whatley <jacob.whatley91@gmail.com>
2015-11-03 11:15:39 -0500

    bullhead: setup CM makefiles
    Change-Id: I3882a8381338427364688bd5e372a07f19d02e92

---
+++ cm.mk
@@ -0,0 +1,24 @@
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1920
+TARGET_SCREEN_WIDTH := 1080
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+# Enhanced NFC
+$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/lge/bullhead/aosp_bullhead.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_NAME := cm_bullhead
+PRODUCT_BRAND := google
+PRODUCT_MODEL := Nexus 5x
+TARGET_MANUFACTURER := LGE
+PRODUCT_RESTRICT_VENDOR_FILES := false
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=bullhead \
+    BUILD_FINGERPRINT="google/bullhead/bullhead:6.0/MDB08M/2353240:user/release-keys" \
+    PRIVATE_BUILD_DESC="bullhead-user 6.0 MDB08M 2353240 release-key"
