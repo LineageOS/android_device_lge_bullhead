b2d8bf99ea8e2d5bca27d323e0324adc8a24a930
Abhisek Devkota <ciwrl@cyanogenmod.com>
2015-11-13 08:01:11 -0800

    Remove Launcher3 reference
    Change-Id: I22d654e0ce9689ced734a9b84181637964312f1a

--- aosp_bullhead.mk
+++ aosp_bullhead.mk
@@ -35,7 +35,3 @@ PRODUCT_COPY_FILES += device/lge/bullhead/fstab.aosp_bullhead:root/fstab.bullhea
 
 $(call inherit-product, device/lge/bullhead/device.mk)
 $(call inherit-product-if-exists, vendor/lge/bullhead/device-vendor.mk)
-
-PRODUCT_PACKAGES += \
-    Launcher3
-
