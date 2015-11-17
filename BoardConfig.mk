10dc1e2af2f4a9e824fb61a6eb83f0746a398856
Jake Whatley <jacob.whatley91@gmail.com>
2015-11-17 01:11:08 -0500

    bullhead: redirect /vendor to /system
    Change-Id: I576d8cd07cdf086dbb47623fb64aa41ec87360c9

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -134,7 +134,7 @@ BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE := ext4
 BOARD_FLASH_BLOCK_SIZE := 131072
 
 # Build a separate vendor.img
-TARGET_COPY_OUT_VENDOR := vendor
+TARGET_COPY_OUT_VENDOR := system
 
 TARGET_RECOVERY_FSTAB = device/lge/bullhead/fstab.bullhead
 
