d2264a5fe090389452ae071878aa01075d8e986c
smoki3 <smoki3@gmx.de>
2016-02-27 12:43:16 +0100

    Bullhead: Enable Snap Camera
    Change-Id: Ib8a133f1c64d7c380778a1e71000a97d9fd24817

--- device.mk
+++ device.mk
@@ -202,7 +202,8 @@ PRODUCT_PACKAGES += \
     libmmcamera_interface2 \
     libmmjpeg_interface \
     libqomx_core \
-    mm-qcamera-app
+    mm-qcamera-app \
+    Snap
 
 # Sensor & activity_recognition HAL
 PRODUCT_PACKAGES += \
