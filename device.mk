6d22f0d8600bc58e1ed84e9826fac1d0fde4b483
smoki3 <kevinbayer8@gmail.com>
2016-03-03 18:42:40 +0100

    bullhead: Enable Snap
    Change-Id: I80d3185f9802948c77e37c11dc32bb14ef023187
    Signed-off-by: Paul Keith <javelinanddart@gmail.com>

--- device.mk
+++ device.mk
@@ -223,7 +223,12 @@ PRODUCT_PACKAGES += \
     libmmcamera_interface2 \
     libmmjpeg_interface \
     libqomx_core \
-    mm-qcamera-app
+    mm-qcamera-app \
+    Snap
+
+# Snap Configuration
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.camera.cpp.duplication=false
 
 # Sensor & activity_recognition HAL
 TARGET_USES_NANOHUB_SENSORHAL := true
