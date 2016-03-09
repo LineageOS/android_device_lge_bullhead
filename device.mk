0a0162cfa698745c73c07f32c194eae79cdcf2a5
Steve Kondik <steve@cyngn.com>
2016-03-08 17:39:20 -0800

    bullhead: Add default output session effects
     * Attach the music_helper by default to the output session. This
       is enough to trigger the policy to send notifications to
       apps listening for them. This will allow effects to be attached
       to any stream.
     * NOTE: This configuration is different than the Google-supplied
       configuration which lives in the vendor partition. Google's
       default configuration uses the Franhaufer FMAS module, which
       I have skipped here since it does not work with offload and
       doesn't really sound that great.
    
    Change-Id: I91197ee5e913c9418522a4ee0e3b4d4c4f2e2804

--- device.mk
+++ device.mk
@@ -39,7 +39,7 @@ PRODUCT_COPY_FILES += \
 # Audio
 PRODUCT_COPY_FILES += \
     device/lge/bullhead/audio_policy.conf:system/etc/audio_policy.conf \
-    device/lge/bullhead/audio_effects.conf:system/etc/audio_effects.conf \
+    device/lge/bullhead/audio_effects.conf:system/etc/audio_effects_vendor.conf \
     device/lge/bullhead/mixer_paths.xml:system/etc/mixer_paths.xml \
     device/lge/bullhead/audio_platform_info.xml:system/etc/audio_platform_info.xml
 
