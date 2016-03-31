89a2ad56ac33fc30c68c18be9c320f6431f05665
Abhisek Devkota <ciwrl@cyanogenmod.com>
2016-03-31 13:12:02 -0700

    Resolve incorrect verity assumptions
    Change-Id: Ied6e47960238d0c26cb82cf5c6ac94f8e810fd29

--- device.mk
+++ device.mk
@@ -559,7 +559,7 @@ endif
 
 # setup dm-verity configs.
 PRODUCT_SYSTEM_VERITY_PARTITION := /dev/block/platform/soc.0/f9824900.sdhci/by-name/system
-PRODUCT_VENDOR_VERITY_PARTITION := /dev/block/platform/soc.0/f9824900.sdhci/by-name/vendor
+#PRODUCT_VENDOR_VERITY_PARTITION := /dev/block/platform/soc.0/f9824900.sdhci/by-name/vendor
 $(call inherit-product, build/target/product/verity.mk)
 
 # OEM Unlock reporting
