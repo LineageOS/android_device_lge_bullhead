6c8ffd17ad3702c00bb348d1b0d572efd70b0821
Joshndroid <fmxjoshiewah@gmail.com>
2016-05-01 17:06:44 +1000

    bullhead: Enable Gello
    Change-Id: I292b9476d5a87775b8df24a0e1f737a81f008561
    Signed-off-by: Paul Keith <javelinanddart@gmail.com>

--- device.mk
+++ device.mk
@@ -193,6 +193,10 @@ PRODUCT_PACKAGES += \
     librmnetctl \
     rmnetcli
 
+# Gello
+PRODUCT_PACKAGES += \
+    Gello
+
 # GPS configuration
 PRODUCT_COPY_FILES += \
     device/lge/bullhead/gps.conf:system/etc/gps.conf
