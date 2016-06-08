18ad86c61d2a9759db0a4ef01d80ae172d12318b
Akhil Narang <akhilnarang.1999@gmail.com>
2016-06-08 13:47:31 +0530

    bullhead: MTC19V
    Change-Id: I714acefe1ac85bff05cd562b785b43fe550673ed
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- cm.mk
+++ cm.mk
@@ -20,6 +20,6 @@ PRODUCT_RESTRICT_VENDOR_FILES := false
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=bullhead \
-    BUILD_FINGERPRINT=google/bullhead/bullhead:6.0.1/MTC19T/2741993:user/release-keys \
-    PRIVATE_BUILD_DESC="bullhead-user 6.0.1 MTC19T 2741993 release-keys" \
-    BUILD_ID=MTC19T
+    BUILD_FINGERPRINT=google/bullhead/bullhead:6.0.1/MTC19V/2862947:user/release-keys \
+    PRIVATE_BUILD_DESC="bullhead-user 6.0.1 MTC19V 2862947 release-keys" \
+    BUILD_ID=MTC19V
