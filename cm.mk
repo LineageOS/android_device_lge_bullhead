9e4ada43d9e3b77420f66d951a01a388003e60e5
razorloves <razorloves@gmail.com>
2016-08-02 14:37:07 -0700

    bullhead: remove build id override
    Change-Id: I71db98d98bb7590bd0d12a87b745099915276890

--- cm.mk
+++ cm.mk
@@ -21,5 +21,4 @@ PRODUCT_RESTRICT_VENDOR_FILES := false
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=bullhead \
     BUILD_FINGERPRINT=google/bullhead/bullhead:6.0.1/MTC20K/3142008:user/release-keys \
-    PRIVATE_BUILD_DESC="bullhead-user 6.0.1 MTC20K 3142008 release-keys" \
-    BUILD_ID=MTC20K
+    PRIVATE_BUILD_DESC="bullhead-user 6.0.1 MTC20K 3142008 release-keys"
