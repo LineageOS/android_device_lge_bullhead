6657effdc31df276a272be554d3825153db4237d
Keith Mok <kmok@cyngn.com>
2016-09-22 16:04:44 -0700

    bullhead: enable sdcardfs
    Change-Id: I86135782d06375f73c6ec936b0e67cda35795fe5

--- device.mk
+++ device.mk
@@ -424,6 +424,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.camera.notify_nfc=1
 
+# Storage
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.sys.sdcardfs=true
+
 # Power HAL
 PRODUCT_PACKAGES += \
     power.bullhead \
