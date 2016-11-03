35d9028a18954a41492648102f153d9f8a2a712f
Etan Cohen <etancohen@google.com>
2016-11-03 19:59:21 +0000

    Revert "[NAN-AWARE] Rename NAN to Aware [DO NOT MERGE]"
    This reverts commit 5ae056b06c1833a916fd44b0c0169bfef4843e8f.
    
    Change-Id: I9d6de10724ce70737155e514117ea45c58686858

--- device.mk
+++ device.mk
@@ -96,7 +96,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:system/etc/permissions/android.hardware.fingerprint.xml \
     frameworks/native/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
     frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
-    frameworks/native/data/etc/android.hardware.wifi.aware.xml:system/etc/permissions/android.hardware.wifi.aware.xml \
+    frameworks/native/data/etc/android.hardware.wifi.nan.xml:system/etc/permissions/android.hardware.wifi.nan.xml \
     frameworks/native/data/etc/android.hardware.wifi.direct.xml:system/etc/permissions/android.hardware.wifi.direct.xml \
     frameworks/native/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
     frameworks/native/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
