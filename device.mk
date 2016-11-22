e9dedd0da6b5a7461edee933277913aa76f0f2c9
Sasha Levitskiy <sanek@google.com>
2016-11-22 14:07:59 -0800

    Bullhead: Treble: Fingerprint: add passthrough HAL wrapper.
    Bug: 31973669
    Test: Device works with FP sensor.
    Change-Id: I51f5b30740aa5be6de153b851a7b16e1754bb293
    Signed-off-by: Sasha Levitskiy <sanek@google.com>

--- device.mk
+++ device.mk
@@ -263,7 +263,8 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint Sensor
 PRODUCT_PACKAGES += \
-    fingerprint.bullhead
+    fingerprint.bullhead \
+    android.hardware.biometrics.fingerprint@2.1-impl
 
 # Wi-Fi
 PRODUCT_PACKAGES += \
