d8d7d9bb900fd29ad5ba940e20fcb0203fedfeb3
Jeff Tinker <jtinker@google.com>
2017-01-01 11:01:11 -0800

    Add drm HALs to device makefiles
    bug:32815560
    Change-Id: I2c7b26784b8af8fbfb7abf9828d48cc63fdbbf85

--- device.mk
+++ device.mk
@@ -190,6 +190,14 @@ PRODUCT_PACKAGES += \
     android.hardware.light@2.0-impl
 
 PRODUCT_PACKAGES += \
+    android.hardware.drm@1.0-impl \
+
+ifeq ($(ENABLE_TREBLE), true)
+PRODUCT_PACKAGES += \
+    android.hardware.drm@1.0-service
+endif
+
+PRODUCT_PACKAGES += \
     libc2dcolorconvert \
     libstagefrighthw \
     libOmxCore \
