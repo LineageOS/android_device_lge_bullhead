99411e02366975b673b923a530529107e601d56c
razorloves <razorloves@gmail.com>
2017-01-03 01:20:49 +0000

    bullhead: rebrand to LineageOS (2/2)
    Change-Id: I3b11cc76d0bc60c644c5d01a393ceedb5bfce445

--- lineage.mk
+++ lineage.mk
@@ -2,14 +2,14 @@
 TARGET_SCREEN_HEIGHT := 1920
 TARGET_SCREEN_WIDTH := 1080
 
-# Inherit some common CM stuff.
+# Inherit some common LineageOS stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/lge/bullhead/aosp_bullhead.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := cm_bullhead
+PRODUCT_NAME := lineage_bullhead
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Nexus 5X
 TARGET_MANUFACTURER := LGE
