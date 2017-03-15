304d5c3e3848193095101f935f505416685c3f6a
Fabien Sanglard <sanglardf@google.com>
2017-03-15 15:07:06 -0700

    Enable HWComposer service
    Test: AUPT
    Change-Id: Ia679386a2ee3af9d618e7d72f5c23e5fab84c4b7

--- device.mk
+++ device.mk
@@ -234,6 +234,9 @@ PRODUCT_PACKAGES += \
     android.hardware.broadcastradio@1.0-impl \
     android.hardware.soundtrigger@2.0-impl
 
+PRODUCT_PACKAGES += \
+    android.hardware.graphics.composer@2.1-impl
+
 # GPS configuration
 PRODUCT_COPY_FILES += \
     device/lge/bullhead/gps.conf:system/etc/gps.conf
