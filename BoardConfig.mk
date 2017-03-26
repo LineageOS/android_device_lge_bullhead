0e9b6666075eec39203532c80c6a2c5eed835684
Jeff Sharkey <jsharkey@android.com>
2017-03-25 22:56:30 -0600

    Enable partitions on loop devices.
    This is used by the new "virtual disk" feature used by vold to help
    debug and test storage code on devices without physical media.
    
    Bug: 34903607
    Test: builds, boots, virtual disk works
    Change-Id: I57eeeb1492286053f48f90f9799c08efde7c5e35

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -37,6 +37,7 @@ BOARD_RAMDISK_OFFSET     := 0x02000000
 
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=bullhead boot_cpus=0-5
 BOARD_KERNEL_CMDLINE += lpm_levels.sleep_disabled=1 msm_poweroff.download_mode=0
+BOARD_KERNEL_CMDLINE += loop.max_part=7
 
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset $(BOARD_RAMDISK_OFFSET) --tags_offset $(BOARD_KERNEL_TAGS_OFFSET)
 
