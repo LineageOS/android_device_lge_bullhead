4b826a7ad0302250dd0f1fb0cf096eb01216fca5
razorloves <razorloves@gmail.com>
2017-08-07 14:13:53 -0500

    bullhead: update build fingerprint & description to N2G48C
    Change-Id: Idde7957f9fa7bb6bf644235aef5abc98ff900665

--- lineage.mk
+++ lineage.mk
@@ -17,5 +17,5 @@ PRODUCT_RESTRICT_VENDOR_FILES := false
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=bullhead \
-    BUILD_FINGERPRINT="google/bullhead/bullhead:7.1.2/N2G47Z/4045513:user/release-keys" \
-    PRIVATE_BUILD_DESC="bullhead-user 7.1.2 N2G47Z 4045513 release-keys"
+    BUILD_FINGERPRINT="google/bullhead/bullhead:7.1.2/N2G48C/4104010:user/release-keys" \
+    PRIVATE_BUILD_DESC="bullhead-user 7.1.2 N2G48C 4104010 release-keys"
