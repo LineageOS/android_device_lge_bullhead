9c390999ccfd230647f820fc64dc7c7e34894351
razorloves <razorloves@gmail.com>
2017-12-23 20:38:59 -0600

    bulhead: update build fingerprint to oreo mr1 opm1.171019.011
    Change-Id: I8cdadddc8b79228d6c27338ef835f778ad778aa6

--- lineage.mk
+++ lineage.mk
@@ -17,5 +17,5 @@ PRODUCT_RESTRICT_VENDOR_FILES := false
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=bullhead \
-    BUILD_FINGERPRINT="google/bullhead/bullhead:8.0.0/OPR6.170623.013/4283548:user/release-keys" \
-    PRIVATE_BUILD_DESC="bullhead-user 8.0.0 OPR6.170623.013 4283548 release-keys"
+    BUILD_FINGERPRINT="google/bullhead/bullhead:8.1.0/OPM1.171019.011/4448085:user/release-keys" \
+    PRIVATE_BUILD_DESC="bullhead-user 8.1.0 OPM1.171019.011 4448085 release-keys"
