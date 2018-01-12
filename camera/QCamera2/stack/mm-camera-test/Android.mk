f986c1804711e2d9482bf56a4544888715384bf3
Dan Albert <danalbert@google.com>
2018-01-12 13:49:54 -0800

    Don't use the STL in C projects.
    The libc++ update doesn't play nicely with GCC compiling C sources.
    
    (cherry picked from commit c9a5f94e1e4f41025fab1c5608851392d66df964)
    
    Test: mm
    Bug: None
    Merged-In: I4d06c51f9d3396344d4617a6f0b8192f88ce7b87
    Change-Id: I4d06c51f9d3396344d4617a6f0b8192f88ce7b87

--- camera/QCamera2/stack/mm-camera-test/Android.mk
+++ camera/QCamera2/stack/mm-camera-test/Android.mk
@@ -7,6 +7,7 @@ include $(CLEAR_VARS)
 
 # b/24171136 many files not compiling with clang/llvm yet
 LOCAL_CLANG := false
+LOCAL_CXX_STL := none
 
 LOCAL_CFLAGS:= \
         -DAMSS_VERSION=$(AMSS_VERSION) \
@@ -118,6 +119,7 @@ include $(CLEAR_VARS)
 
 # b/24171136 many files not compiling with clang/llvm yet
 LOCAL_CLANG := false
+LOCAL_CXX_STL := none
 
 LOCAL_CFLAGS:= \
         -DAMSS_VERSION=$(AMSS_VERSION) \
@@ -214,7 +216,7 @@ endif
 LOCAL_CFLAGS += -Wall -Wextra -Werror
 
 LOCAL_SHARED_LIBRARIES:= \
-         libcutils libdl libmmcamera_interface
+         libcutils libdl libmmcamera_interface liblog
 
 LOCAL_MODULE_TAGS := optional
 
