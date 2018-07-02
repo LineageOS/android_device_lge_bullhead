000a95b8af1b17cac10c57c7e0fe8235c2b84a27
razorloves <razorloves@gmail.com>
2018-07-02 13:16:45 -0500

    bullhead: update build fingerprint to July 2018 OPM6.171019.030.E1
    Change-Id: I5a30fdfa4a7588c7019dfcb95bb117f7e7b96228

--- lineage.mk
+++ lineage.mk
@@ -16,6 +16,6 @@ TARGET_MANUFACTURER := LGE
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=bullhead \
-    PRIVATE_BUILD_DESC="bullhead-user 8.1.0 OPM6.171019.030.B1 4768815 release-keys"
+    PRIVATE_BUILD_DESC="bullhead-user 8.1.0 OPM6.171019.030.E1 4805388 release-keys"
 
-BUILD_FINGERPRINT := google/bullhead/bullhead:8.1.0/OPM6.171019.030.B1/4768815:user/release-keys
+BUILD_FINGERPRINT := google/bullhead/bullhead:8.1.0/OPM6.171019.030.E1/4805388:user/release-keys
