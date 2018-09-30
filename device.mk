c26beeda03f4d23e8c7764af05b104bb9191829b
Kevin F. Haggerty <haggertk@lineageos.org>
2018-09-30 09:16:34 -0600

    bullhead: Build libhubconnection from source
    Change-Id: I487f474da78944c08b4de0f3140a2c3b64a97285

--- device.mk
+++ device.mk
@@ -298,6 +298,7 @@ PRODUCT_PACKAGES += \
     context_hub.default \
     android.hardware.sensors@1.0-impl \
     android.hardware.contexthub@1.0-impl \
+    libhubconnection \
 
 # new gatekeeper HAL
 PRODUCT_PACKAGES +=                         \
