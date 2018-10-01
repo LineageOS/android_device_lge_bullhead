fc3642679d202a4ff3b7164134460a5d876438b2
razorloves <razorloves@gmail.com>
2018-09-30 22:54:09 -0500

    bullhead: Allow verity on /vendor with user builds
    Partial revert of 89a2ad56ac33fc30c68c18be9c320f6431f05665
    
    Change-Id: I496617f99a2492bda3880dd626871b313ca6a774

--- device.mk
+++ device.mk
@@ -581,7 +581,7 @@ ifeq ($(TARGET_BUILD_VARIANT),user)
 
 # setup dm-verity configs.
 PRODUCT_SYSTEM_VERITY_PARTITION := /dev/block/platform/soc.0/f9824900.sdhci/by-name/system
-#PRODUCT_VENDOR_VERITY_PARTITION := /dev/block/platform/soc.0/f9824900.sdhci/by-name/vendor
+PRODUCT_VENDOR_VERITY_PARTITION := /dev/block/platform/soc.0/f9824900.sdhci/by-name/vendor
 $(call inherit-product, build/target/product/verity.mk)
 endif
 
