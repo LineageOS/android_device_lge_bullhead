ed1a7fba2d4248275cf26d6b94a49ab00ca63d5b
Christopher N. Hesse <raymanfx@gmail.com>
2018-10-16 10:02:53 +0200

    bullhead: Advertise lineage product via AOSP products
    Change-Id: I2d4e2a5b750aa094852fce57a33c39b4933f4b6e

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -16,8 +16,10 @@
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/aosp_bullhead.mk \
-    $(LOCAL_DIR)/aosp_bullhead_svelte.mk
+    $(LOCAL_DIR)/aosp_bullhead_svelte.mk \
+    $(LOCAL_DIR)/lineage_bullhead.mk
 
 COMMON_LUNCH_CHOICES := \
     aosp_bullhead-userdebug \
-    aosp_bullhead_svelte-userdebug
+    aosp_bullhead_svelte-userdebug \
+    lineage_bullhead-userdebug
